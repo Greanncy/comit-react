d3e14fde213c9665a52c40560623566121e25ecc
Greanncy <gr002ramales@gmail.com>
2019-10-28 19:56:29 -0600

    Completed lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,19 @@
+const monthOfBirth = "September";
+const firstName = "Greanncy";
+const lastName = "Ramales";
+
+let city;
+let age;
+let nextCity;
+
+city = "Saskatoon";
+age = "20";
+nextCity = "Saskatoon";
+
+console.log("monthOfBirth", monthOfBirth);
+console.log("firstName", firstName);
+console.log("lastName", lastName);
+console.log("city", city);
+console.log("age", age);
+console.log("nextCity", nextCity);
+console.log(`${firstName} ${lastName}`);
